1e7b940977c7d929a373a290272d4b2fe621821b
GoldenGlint <darhgo@gmail.com>
2026-04-29 14:29:11 -0400

    Controller Function and Code Test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import "./styles.css";
 console.log("Hello World");
 
+class controller{
+    #projectList=[];
+    #projectCounter=0;
+
+    addProject(project){
+        this.#projectList.push(project);
+        this.#projectCounter++;
+    }
+
+    removeProject(ID){
+        this.#projectList=this.#projectList.filter(project=>project.projectID!=ID);
+        this.#projectCounter--;
+    }
+}
+
 class project{
     #itemsList=[];
     #counter=0;
+    #projectID=crypto.randomUUID();
 
     addItem(item){
         this.#itemsList.push(item);
@@ -21,6 +37,10 @@ class project{
 
     get itemsList(){
         return this.#itemsList;
+    }
+
+    get projectID(){
+        return this.#projectID;
     }
     
 }
@@ -81,16 +101,9 @@ class item{
 }
 
 
-
+//Terminal Location: http://localhost:8080
 
 /* Testing Code
-const Darren = new item("Code", "This is a Description", "May 1st", "high");
-console.log(Darren);
-console.log(Darren.priority);
-Darren.priority="low";
-console.log(Darren.priority)
-*/
-
 const Darren = new item("Code", "This is a Description", "May 1st", "high");
 console.log(Darren);
 
@@ -103,3 +116,27 @@ p.addItem(Jason);
 
 console.log(p.counter);
 console.log(p.itemsList);
+
+const control= new controller();
+control.addProject(p);
+console.log(control);
+
+
+
+const a = new item("a", "This is a fd", "fdsfs 1st", "fd");
+console.log(Darren);
+
+const b = new item("fd", "This fds", "May 2nfdsd", "fl");
+console.log(Jason);
+
+const c=new project();
+c.addItem(a);
+c.addItem(b);
+control.addProject(c);
+console.log(control);
+p.removeItem(Darren.getID);
+console.log(p);
+control.removeProject(c.projectID);
+console.log(control);
+*/
+
