56376732d663e2e646fe5726254398be7ec5a082
GoldenGlint <darhgo@gmail.com>
2026-04-30 01:34:41 -0400

    File Structure, and also render
    Next steps to connect imports and render home page

--- src/render.js
+++ src/render.js
@@ -2,7 +2,41 @@
 const render = {
     
     sidebar(arr){
+        const sidebarContainer=document.createElement("div");
+        sidebarContainer.id="sidebar-container";
 
+        const mainButtonContainer=document.createElement("div");
+        mainButtonContainer.id="main-buttons-container";
+
+        const inbox=document.createElement("button");
+        inbox.id="inbox";
+        inbox.textContent="Inbox";
+
+        const today=document.createElement("button");
+        inbox.id="today";
+        inbox.textContent="Today";
+
+        const thisWeek=document.createElement("button");
+        inbox.id="week";
+        inbox.textContent="This Week";
+
+        mainButtonContainer.append(inbox, today, thisWeek);
+
+        const projects=document.createElement("projects");
+        mainButtonContainer.id="projects";
+
+        const projectOverview=document.createElement("button");
+        inbox.id="project-overview";
+        inbox.textContent="Project Overview";
+
+        for(const i=0; i<arr.size(); i++){
+            const project=document.createElement("button");
+            project.id=arr[i].getID;
+            project.textContent=arr[i].name;
+            project.classList.add("project-button");
+            projects.appendChild(project);
+        }
+        sidebarContainer.append(mainButtonContainer, projects);
     },
     mainHeader(num){
         const header=document.querySelector("#header-container");
@@ -19,6 +53,8 @@ const render = {
         const button=document.createElement("button");
         button.id="toggle";
         button.textContent="Dark Mode";
+
+        header.append(title, counter, button);
     },
     projectOverview(){
 
